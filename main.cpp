f825b901435380c1219ee10ef26d534d7252fcaf
Johan <Johan@172.29.45.105>
2019-07-10 16:00:05 +0200

    Added wait for stop action

--- main.cpp
+++ main.cpp
@@ -94,7 +94,7 @@ int main(int argc, char *argv[])
 		}
 		//BaseThread::mutexLog.unlock();
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-		break;
+		//break;
 	#elif OS == OS_WINDOWS
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 		break;
