16b22861428f5a59c9190d76f4d1b8d5bf0cbb88
Johan <Johan@172.29.45.105>
2019-07-11 17:35:57 +0200

    Added initial filter computing

--- pnp_thread.cpp
+++ pnp_thread.cpp
@@ -116,9 +116,11 @@ PNPThread::PNPThread(double fl, HoughThread* ht): m_web_string_stream(std::ios_b
 	//this->m_current_filter_centers[3][0] = 45;
 	//this->m_current_filter_centers[3][1] = 19;
 
-	this->m_ht->activateTracking();
-
 	this->computeLineIntersection();
+	this->computePosit();
+	this->updateFilteringArray();
+	this->m_web_string_stream << "{\"tracking\": true},";
+	this->m_ht->activateTracking();
 }
 
 PNPThread::~PNPThread() {
