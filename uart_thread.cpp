284e535419ea0989e7fd741d02e9b5e27d07b09f
Johan <Johan@172.29.45.105>
2019-07-15 16:02:49 +0200

    Updated online BAF Going back online

--- uart_thread.cpp
+++ uart_thread.cpp
@@ -285,7 +285,7 @@ void UARTThread::threadFunction() {
 					event_sended_global++;
 					this->m_ht->addEvent(x,y,p==1,t);
 				}
-				if(tx > 0 && tx < this->m_camera_x-1 && ty > 0 && ty < this->m_camera_y-1)
+				if(tx > 0 && tx < (this->m_camera_x>>1)-1 && ty > 0 && ty < (this->m_camera_y>>1)-1)
 				{
 					this->m_baf_time_array[tx+1][ty+1] = t;
 					this->m_baf_time_array[tx  ][ty+1] = t;
@@ -365,7 +365,7 @@ void UARTThread::threadFunction() {
 					event_sended_global++;
 					this->m_ht->addEvent(x,y,p==1,t);
 				}
-				if(tx > 0 && tx < this->m_camera_x-1 && ty > 0 && ty < this->m_camera_y-1)
+				if(tx > 0 && tx < (this->m_camera_x>>1)-1 && ty > 0 && ty < (this->m_camera_y>>1)-1)
 				{
 					this->m_baf_time_array[tx+1][ty+1] = t;
 					this->m_baf_time_array[tx  ][ty+1] = t;
