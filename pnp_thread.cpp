6f2fd470b6ed8d3806cf2b3be362cba527389134
Johan <Johan@172.29.45.105>
2019-07-16 13:59:56 +0200

    Added all log distance

--- pnp_thread.cpp
+++ pnp_thread.cpp
@@ -424,7 +424,10 @@ void PNPThread::computeEvent(double theta, double dist, unsigned int t, int line
 					double theta = this->m_line_parameters[i][0];
 					double dist = this->m_line_parameters[i][1];
 					this->mutexLog.lock();
-					std::cout << "Dist cycl: " << std::min(std::fmod(theta-PI/2+2*PI,2*PI), std::fmod(PI/2-theta+2*PI,2*PI)) << " PI/2 " << theta << std::endl;
+					std::cout << "Dist cycl: " << std::min(std::fmod(theta-PI/2+2*PI,2*PI), std::fmod(PI/2-theta+2*PI,2*PI)) << "   PI/2 " << theta << std::endl;
+					std::cout << "Dist cycl: " << std::min(std::fmod(theta-3*PI/2+2*PI,2*PI), std::fmod(3*PI/2-theta+2*PI,2*PI)) << " 3*PI/2 " << theta << std::endl;
+					std::cout << "Dist cycl: " << std::min(std::fmod(theta-5*PI/2+2*PI,2*PI), std::fmod(5*PI/2-theta+2*PI,2*PI)) << " 5*PI/2 " << theta << std::endl;
+					std::cout << "Dist cycl: " << std::min(std::fmod(theta-7*PI/2+2*PI,2*PI), std::fmod(7*PI/2-theta+2*PI,2*PI)) << " 7*PI/2 " << theta << std::endl;
 					this->mutexLog.unlock();
 					if(std::min(std::fmod(theta-PI/2+2*PI,2*PI), std::fmod(PI/2-theta+2*PI,2*PI)) < PI/4)
 					{
