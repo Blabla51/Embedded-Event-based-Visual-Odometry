ba220429b79c08c8250ea72c9f1d95f231c08271
Johan <Johan@10.0.1.56>
2019-08-05 16:31:17 +0200

    Object matrix corrected

--- pnp_thread.cpp
+++ pnp_thread.cpp
@@ -137,11 +137,11 @@ PNPThread::PNPThread(double fl, HoughThread* ht)
 	m_object_matrix[0][0] =  0.0;
 	m_object_matrix[1][0] = -2.0/3.0*10;
 	m_object_matrix[2][0] = -1.0/3.0*10;
-	m_object_matrix[3][0] = -1.0/3.0*10;
+	m_object_matrix[3][0] =  1.0/3.0*10;
 	m_object_matrix[0][1] =  0.0;
 	m_object_matrix[1][1] =  1.0/3.0*10;
 	m_object_matrix[2][1] = -1.0/3.0*10;
-	m_object_matrix[3][1] =  2.0/3.0*10;
+	m_object_matrix[3][1] = -2.0/3.0*10;
 	m_object_matrix[0][2] =  0.0;
 	m_object_matrix[1][2] =  0.0;
 	m_object_matrix[2][2] =  0.0;
